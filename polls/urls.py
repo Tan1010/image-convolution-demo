e254569d07cdb20432e053d7fcfd555e071fbba2
Tan1010 <58654063+Tan1010@users.noreply.github.com>
2023-03-07 00:14:35 +0800

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('convolve', views.convolve, name='convolve'),
+    path('<slug:slug>', views.deleteImage, name='deleteImage'),
 ]
